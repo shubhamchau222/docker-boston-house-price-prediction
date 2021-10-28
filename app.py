15a17bdc14092571814ef913d1a04689c736a3bf
Shubham Chaudhari <shubhamchau78@gmail.com>
2021-10-28 16:02:52 +0530

    app.py upadated

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def index():
         return render_template('Index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True , host="0.0.0.0" , port="8080")
-    # app.run(debug=True )
+    #app.run(debug=True , host="0.0.0.0" , port="8080")
+    app.run(debug=True )
 
 
 
